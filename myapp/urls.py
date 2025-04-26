34f3cc1a3b4b916b36a7a762751fecbd871b992c
Алексей <azakharche@gmail.com>
2025-04-26 15:56:43 +0300

    CBV

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,8 +6,10 @@ app_name= 'myapp'
 
 
 urlpatterns = [
-    path('',views.index,name='index'),
-    path('<int:my_id>/',views.current_index, name='detail'),
+    # path('',views.index,name='index'),
+    path('',views.ProductListView.as_view(),name='index'),
+    # path('<int:my_id>/',views.current_index, name='detail'),
+    path('<int:pk>/',views.ProductDetailView.as_view(), name='detail'),
     path('additem/', views.add_item, name="additem"),
     path('updateitem/<int:my_id>/', views.update_item, name="update_item"),
     path('deleteitem/<int:my_id>/', views.delete_item, name="delete_item"),
